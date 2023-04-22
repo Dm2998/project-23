335c5524d46698793bcf4bcbd4142c603997c976
Diego <Dm2998@github.com>
2023-04-22 11:58:47 +0000

    test

---
+++ FinalVersionProject/README.md
@@ -0,0 +1,30 @@
+﻿# Bike Rental Website with Supabase Integration
+
+A responsive, user-friendly bike rental website that that allows users to browse, book, subscribe to newsletters and manage bike rentals, featuring an interactive map connected to the Google Maps API for an enhanced browsing experience. All data is managed through Supabase, a scalable and powerful backend-as-a-service.
+
+## Features
+
+- Browse available bikes
+- Newsletter subscription 
+- Mobile-responsive design
+- User registration and login using Supabase
+- Integration with Google Maps API for map browsing
+- Display bike rental locations fetched from the database on the map
+- Book and manage bike rentals 
+
+## Technologies Used
+
+- HTML, CSS, JavaScript
+- Supabase for backend data storage and management
+- Other libraries and frameworks (e.g., jQuery, Owl Carousel, etc.)
+- Google Maps API for map integration
+
+
+
+
+
+
+
+
+
+
---
+++ FinalVersionProject/public/scripts/navbarScript.js
@@ -0,0 +1,30 @@
+// Immediately invoked function expression (IIFE) to create a local scope and avoid polluting the global scope
+(function () {
+  // Navbar JavaScript
+  var menuHolder = document.getElementById('menuHolder');
+  var siteBrand = document.getElementById('siteBrand');
+
+  // Function to toggle the 'drawMenu' class on the menuHolder element
+  function menuToggle() {
+    menuHolder.classList.toggle("drawMenu");
+  }
+
+  // Function to update the site brand text based on the window width
+  function updateSiteBrand() {
+    if (window.innerWidth < 420) {
+      siteBrand.textContent = "MAS";
+    } else {
+      siteBrand.textContent = "Amigo Bike Rental";
+    }
+  }
+
+  // Update siteBrand text on page load
+  updateSiteBrand();
+
+  // Attach event listeners
+  // Update siteBrand text when the window is resized
+  window.addEventListener('resize', updateSiteBrand);
+
+  // Expose the menuToggle function to the global scope
+  window.menuToggle = menuToggle;
+})();
